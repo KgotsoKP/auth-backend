836d750f6c3a3a7f1617f48c17f510018b36dca5
Kgotso Phiri <kgotsokg12@gmail.com>
2023-07-11 20:29:08 +0200

    Add jwt utils

---
+++ src/utils/jwt-utils.js
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken';
+import environmemt from '../config/environment';
+
+class JWTUtils{
+    static generateAccessToken(payload, options ={}){
+        const {expiresIn = '1d'} = options;
+        return jwt.sign(payload,environmemt.jwtAccessToeknSecret, {expiresIn})
+    }
+
+    static generateRefreshToken(payload){
+        return jwt.sign(payload,environmemt.jwtRefreshTokenSecret);
+    }
+
+    static verifyAccessToken(accessToken){
+        return jwt.verify(accessToken,environmemt.jwtAccessToeknSecret)
+    }
+
+    static verifyRefreshToken(refreshToken){
+        return jwt.verify(refreshToken,environmemt.jwtRefreshTokenSecret)
+    }
+}
+
+export default JWTUtils;
